609f5758eb74f0114cac5f6864287a68314a5854
g888 DIT UPM <g888@l147.lab.dit.upm.es>
2018-09-13 16:02:55 +0200

    Falta slider

--- Hola Mundo/ViewController.swift
+++ Hola Mundo/ViewController.swift
@@ -7,9 +7,16 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var msgLabel: UILabel!
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    let slider = UISlider()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +26,32 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func updateHola(_ sender: UIButton) {
+        
+        let center = CLLocationCoordinate2D(latitude: 40.6892, longitude: -74.0445)
+        let span = MKCoordinateSpan(latitudeDelta: 0.004, longitudeDelta: 0.004)
+        let reg = MKCoordinateRegion(center:center, span:span)
+        mapView.mapType = MKMapType.hybrid
+        mapView.setRegion(reg, animated: true)
+        
+        slider.value = 5.0
+    }
+    @IBAction func updateMundo(_ sender: UIButton) {
+        
+        mapView.mapType = MKMapType.satellite
+    }
+    @IBAction func updateAlpha(_ sender: UISlider) {
+        msgLabel.alpha = CGFloat(sender.value)
+    }
+    
+    @IBAction func updateSol(_ sender: UIButton) {
+        
+        let center = CLLocationCoordinate2D(latitude: 40.4169, longitude: -3.7033)
+        let span = MKCoordinateSpan(latitudeDelta: 0.004, longitudeDelta: 0.004)
+        let reg = MKCoordinateRegion(center:center, span:span)
+        
+        mapView.setRegion(reg, animated: true)
+    }
 }
 
